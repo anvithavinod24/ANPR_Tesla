00c2c63b818e5a6a54d4a7148f6e40e2d383aa89
anvithavinod24 <anvithavinod24@gmail.com>
2024-02-20 19:11:36 +0530

    ANPR Framework

--- main.py
+++ main.py
@@ -3,80 +3,110 @@ import os
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import easyocr
 
 import util
 
 
 # define constants
-model_cfg_path = os.path.join('.', 'model', 'cfg', 'yolov3.cfg')
-model_weights_path = os.path.join('.', 'model', 'weights', 'yolov3.weights')
+model_cfg_path = os.path.join('.', 'model', 'cfg', 'darknet-yolov3.cfg')
+model_weights_path = os.path.join('.', 'model', 'weights', 'model.weights')
 class_names_path = os.path.join('.', 'model', 'class.names')
 
-img_path = './pexels-diana-huggins-615369.jpg'
+input_dir = r"C:\Users\v\Desktop\carplate"
+for img_name in os.listdir(input_dir):
+    img_path = os.path.join(input_dir,img_name)
 
-# load class names
-with open(class_names_path, 'r') as f:
-    class_names = [j[:-1] for j in f.readlines() if len(j) > 2]
-    f.close()
 
-# load model
-net = cv2.dnn.readNetFromDarknet(model_cfg_path, model_weights_path)
 
-# load image
+    # load class names
+    with open(class_names_path, 'r') as f:
+        class_names = [j[:-1] for j in f.readlines() if len(j) > 2]
+        f.close()
 
-img = cv2.imread(img_path)
+    # load model
+    net = cv2.dnn.readNetFromDarknet(model_cfg_path, model_weights_path)
 
-H, W, _ = img.shape
+    # load image
 
-# convert image
-blob = cv2.dnn.blobFromImage(img, 1 / 255, (320, 320), (0, 0, 0), True)
+    img = cv2.imread(img_path)
 
-# get detections
-net.setInput(blob)
+    H, W, _ = img.shape
 
-detections = util.get_outputs(net)
+    # convert image
+    blob = cv2.dnn.blobFromImage(img, 1 / 255, (416, 416), (0, 0, 0), True)
 
-# bboxes, class_ids, confidences
-bboxes = []
-class_ids = []
-scores = []
+    # get detections
+    net.setInput(blob)
 
-for detection in detections:
-    # [x1, x2, x3, x4, x5, x6, ..., x85]
-    bbox = detection[:4]
+    detections = util.get_outputs(net)
 
-    xc, yc, w, h = bbox
-    bbox = [int(xc * W), int(yc * H), int(w * W), int(h * H)]
+    # bboxes, class_ids, confidences
+    bboxes = []
+    class_ids = []
+    scores = []
 
-    bbox_confidence = detection[4]
+    reader= easyocr.Reader(['en'])
 
-    class_id = np.argmax(detection[5:])
-    score = np.amax(detection[5:])
+    for detection in detections:
+        # [x1, x2, x3, x4, x5, x6, ..., x85]
+        bbox = detection[:4]
 
-    bboxes.append(bbox)
-    class_ids.append(class_id)
-    scores.append(score)
+        xc, yc, w, h = bbox
+        bbox = [int(xc * W), int(yc * H), int(w * W), int(h * H)]
 
-# apply nms
-bboxes, class_ids, scores = util.NMS(bboxes, class_ids, scores)
+        bbox_confidence = detection[4]
 
-# plot
+        class_id = np.argmax(detection[5:])
+        score = np.amax(detection[5:])
 
-for bbox_, bbox in enumerate(bboxes):
-    xc, yc, w, h = bbox
+        bboxes.append(bbox)
+        class_ids.append(class_id)
+        scores.append(score)
 
-    cv2.putText(img,
-                class_names[class_ids[bbox_]],
-                (int(xc - (w / 2)), int(yc + (h / 2) - 20)),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                7,
-                (0, 255, 0),
-                15)
-    img = cv2.rectangle(img,
-                        (int(xc - (w / 2)), int(yc - (h / 2))),
-                        (int(xc + (w / 2)), int(yc + (h / 2))),
-                        (0, 255, 0),
-                        10)
+    # apply nms
+    bboxes, class_ids, scores = util.NMS(bboxes, class_ids, scores)
 
-plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-plt.show()
+    # plot
+
+    for bbox_, bbox in enumerate(bboxes):
+        xc, yc, w, h = bbox
+        """
+        cv2.putText(img,
+                    class_names[class_ids[bbox_]],
+                    (int(xc - (w / 2)), int(yc + (h / 2) - 20)),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    7,
+                    (0, 255, 0),
+                    15)
+        """
+        license_plate = img[int(yc - (h / 2)):int(yc + (h / 2)), int(xc - (w / 2)):int(xc + (w / 2)), :].copy()
+        img = cv2.rectangle(img,
+                            (int(xc - (w / 2)), int(yc - (h / 2))),
+                            (int(xc + (w / 2)), int(yc + (h / 2))),
+                            (0, 255, 0),
+                            10)
+
+        license_plate_gray = cv2.cvtColor(license_plate, cv2.COLOR_BGR2GRAY)
+        _, license_plate_thresh = cv2.threshold(license_plate_gray,66,255,cv2.THRESH_BINARY_INV)
+
+        output = reader.readtext(license_plate_gray)
+        for out in output:
+            text_bbox, text, text_score = out
+            print(text,text_score)
+
+    plt.figure()
+    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+
+    """
+    plt.figure()
+    plt.imshow(cv2.cvtColor(license_plate, cv2.COLOR_BGR2RGB))
+
+    plt.figure()
+    plt.imshow(cv2.cvtColor(license_plate_gray, cv2.COLOR_BGR2RGB))
+    """
+    plt.figure()
+    plt.imshow(cv2.cvtColor(license_plate_thresh, cv2.COLOR_BGR2RGB))
+
+
+    plt.show()
